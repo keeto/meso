a4bcff76708f16ae3eb5c9c649ccc707391ef351
Mark Obcena <markeeto@gmail.com>
2011-01-17 21:17:39 +0800

    * [v8cgi-fs] Separating File and Directory.

--- Source/engines/v8cgi/0.8.2/fs.js
+++ Source/engines/v8cgi/0.8.2/fs.js
@@ -8,40 +8,27 @@ var getCwd = exports.getCwd = function getCwd(){
 };
 
 
-// File Class
-var _File = exports.File = new Class({
+// Directory Class
+var _Directory = exports.Directory = new Class({
 
-	Extends: fscommon.File,
+	Extends: fscommon.Directory,
 
-	resolve: function(path, skipCanon){
+	resolve: function(path){
 		var file,
 			cwd = system.getcwd();
-		if (!skipCanon) path = fscommon.toCanonical(path || cwd, cwd);
-		file = new File(path);
-		if (file.exists() && !file.isFile()) return this.resolveDir(path, true);
-		this.$path = path;
-		this.$file = file;
-		return this;
-	},
-
-	resolveDir: function(path, skipCanon){
-		var file,
-			cwd = system.getcwd();
-		if (!skipCanon) path = fscommon.toCanonical(path || cwd, cwd);
+		path = fscommon.toCanonical(path || cwd, cwd);
 		file = new Directory(path);
-		if (file.exists() && !file.isDirectory()) return this.resolve(path, true);
+		if (file.exists() && !file.isDirectory()) throw new Error('Path "'+path+'" is not a directory.');
 		this.$path = path;
 		this.$file = file;
 		return this;
 	},
 
 	remove: function(recursive){
-		if (!this.exists()) return this.onRemoveError(new Error('File does not exists.'));
+		if (!this.exists()) return this.onRemoveError(new Error('Directory does not exists.'));
 		var self = this,
-			File = this.$constructor,
-			path = this.toCanonical(),
+			path = this.$path,
 			file = this.$file;
-
 		var handler = function(){
 			try {
 				file.remove();
@@ -50,13 +37,19 @@ var _File = exports.File = new Class({
 				self.onRemoveError(e);
 			}
 		};
-
-		if (recursive && this.isDirectory()){
+		if (recursive){
 			var dir = this.clone();
 			dir.addEvents({
 				list: function(contents){
 					contents.each(function(item){
-						new File(fscommon.toCanonical(item, path)).remove(true);
+						var file;
+						try {
+							file = new _Directory(fscommon.toCanonical(item, path));
+						} catch(e){
+							file = new _File(fscommon.toCanonical(item, path));
+						} finally {
+							file.remove(true);
+						}
 					});
 					handler();
 				},
@@ -64,6 +57,161 @@ var _File = exports.File = new Class({
 			}).list();
 		} else {
 			handler();
+		}
+		return this;
+	},
+
+	move: function(to){
+		if (to == null) return this.onMoveError(new Error('Destination argument `to` is required.'));
+		if (!this.exists()) return this.onMoveError(new Error('Directory does not exists.'));
+		var path = this.$path;
+		to = fscommon.toCanonical(to, system.getcwd());
+		// Cheating because v8cgi doesn't have Directory move
+		try {
+			var proc = new (require('process').Process),
+				command = ['mv', '"' + path + '"', '"' + to + '"'].join(' '),
+				results = proc.exec2(command);
+
+			if (results.err !== '') throw new Error(results.err);
+			this.onMove(to);
+		} catch(e){
+			this.onMoveError(e);
+		}
+		return this;
+	},
+
+	exists: function(){
+		return this.$file.exists();
+	},
+
+	isDirectory: function(){
+		var result,
+			file = this.$file;
+		if (file.exists() && file.isDirectory){
+			return file.isDirectory();
+		} else {
+			result = false;
+		}
+		return result;
+	},
+
+	isFile: function(){
+		var result,
+			file = this.$file;
+		if (file.exists() && file.isFile){
+			return file.isFile();
+		} else {
+			result = false;
+		}
+		return result;
+	},
+
+	getStat: function(){
+		if (!this.exists()) return this.onStatError(new Error('Directory does not exists.'));
+		var file = this.$file;
+		try {
+			var stat = file.stat();
+			if (stat) return this.onStat({size: stat.size, modified: stat.mtime});
+			throw new Error('Cannot read stats.');
+		} catch(e){
+			this.onStatError(e);
+		}
+		return this;
+	},
+
+	list: function(){
+		if (!this.exists()) return this.onListError(new Error('Directory does not exists.'));
+		var file = this.$file,
+			results = [];
+		try {
+			results.append(file.listFiles());
+			results.append(file.listDirectories());
+			results.sort();
+			this.onList(results);
+		} catch(e){
+			this.onListError(e);
+		}
+		return this;
+	},
+
+	create: function(recurse){
+		var self = this;
+		if (this.exists()){
+			return this.onCreate();
+		} else {
+			var file = this.$file,
+				parent = this.getParent();
+			function handler(){
+				try {
+					file.create();
+					self.onCreate();
+				} catch(e){
+					self.onCreateError(e);
+				}
+			}
+			if (recurse){
+				parent.addEvents({
+					create: handler,
+					createError: this.onCreateError.bind(this)
+				}).create(recurse);
+			} else {
+				handler();
+			}
+		}
+		return this;
+	},
+
+	makeDir: function(dir, recurse){
+		if (dir == null) return this.onMakeDirError('Argument dirName is required.');
+		var file = new this.$constructor(fscommon.toCanonical(dir, this.toCanonical()));
+		file.addEvents({
+			create: this.onMakeDir.bind(this),
+			createError: this.onMakeDirError.bind(this)
+		}).create(recurse);
+		return this;
+	}
+
+});
+
+
+// File Class
+var _File = exports.File = new Class({
+
+	Extends: fscommon.File,
+
+	resolve: function(path){
+		var file,
+			cwd = system.getcwd();
+		path = fscommon.toCanonical(path || cwd, cwd);
+		file = new File(path);
+		if (file.exists() && !file.isFile()) throw new Error('Path "'+path+'" is not a directory.');
+		this.$path = path;
+		this.$file = file;
+		return this;
+	},
+
+	remove: function(){
+		if (!this.exists()) return this.onRemoveError(new Error('File does not exists.'));
+		var file = this.$file;
+		try {
+			file.remove();
+			this.onRemove();
+		} catch(e){
+			this.onRemoveError(e);
+		}
+		return this;
+	},
+
+	move: function(to){
+		if (to == null) return this.onMoveError(new Error('Destination argument `to` is required.'));
+		if (!this.exists()) return this.onMoveError(new Error('File does not exists.'));
+		var file = this.$file;
+		to = fscommon.toCanonical(to, system.getcwd());
+		try {
+			file.move(to);
+			this.onMove(to);
+		} catch(e){
+			this.onMoveError(e);
 		}
 		return this;
 	},
@@ -103,67 +251,11 @@ var _File = exports.File = new Class({
 		var file = this.$file;
 		try {
 			var stat = file.stat();
-			if (stat) return this.onStat({size: stat.size, modified: stat.modified});
+			if (stat) return this.onStat({size: stat.size, modified: stat.mtime});
 			throw new Error('Cannot read stats.');
 		} catch(e){
 			this.onStatError(e);
 		}
-		return this;
-	},
-
-	// Dir Functions
-
-	list: function(){
-		if (!this.exists()) return this.onListError(new Error('File does not exists.'));
-		if (this.isFile()) return this.onListError(new Error('File is not a directory.'));
-		var file = this.$file,
-			results = [];
-		try {
-			results.append(file.listFiles());
-			results.append(file.listDirectories());
-			results.sort();
-			this.onList(results);
-		} catch(e){
-			this.onListError(e);
-		}
-		return this;
-	},
-
-	createDir: function(recurse){
-		var self = this;
-		if (this.exists()){
-			if (this.isFile()) return this.onCreateDirError(new Error('File is not a directory'));
-			return this.onCreateDir();
-		} else {
-			this.resolveDir(this.toCanonical(), true);
-			var file = this.$file,
-				parent = this.getParent();
-			function handler(){
-				try {
-					file.create();
-					self.onCreateDir();
-				} catch(e){
-					self.onCreateDirError(e);
-				}
-			}
-			if (recurse){
-				parent.addEvents({
-					createDir: handler,
-					createDirError: this.onCreateDirError.bind(this)
-				}).createDir(recurse);
-			} else {
-				handler();
-			}
-		}
-		return this;
-	},
-
-	makeDir: function(dir, recurse){
-		var file = new this.$constructor(fscommon.toCanonical(dir, this.toCanonical()));
-		file.addEvents({
-			createDir: this.onMakeDir.bind(this),
-			createDirError: this.onMakeDirError.bind(this)
-		}).createDir(recurse);
 		return this;
 	},
 
@@ -194,43 +286,6 @@ var _File = exports.File = new Class({
 			this.onWrite(data);
 		} catch(e){
 			this.onWriteError(e, data);
-		}
-		return this;
-	},
-
-	move: function(to){
-		if (to == null) return this.onMoveError(new Error('Destination argument `to` is required.'));
-		if (!this.exists()) return this.onMoveError(new Error('File does not exists.'));
-		if (this.isDirectory()) return this.moveDir(to);
-		if (this.isFile()) return this.moveFile(to);
-		return this;
-	},
-
-	moveFile: function(to){
-		var file = this.$file;
-		to = fscommon.toCanonical(to, system.getcwd());
-		try {
-			file.move(to);
-			this.onMove(to);
-		} catch(e){
-			this.onMoveError(e);
-		}
-		return this;
-	},
-
-	moveDir: function(to){
-		var path = this.$path;
-		to = fscommon.toCanonical(to, system.getcwd());
-		// Cheating because v8cgi doesn't have Directory move
-		try {
-			var proc = new (require('process').Process),
-				command = ['mv', '"' + path + '"', '"' + to + '"'].join(' '),
-				results = proc.exec2(command);
-
-			if (results.err !== '') throw new Error(results.err);
-			this.onMove(to);
-		} catch(e){
-			this.onMoveError(e);
 		}
 		return this;
 	}
